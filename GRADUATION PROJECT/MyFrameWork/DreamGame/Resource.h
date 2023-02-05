ed74ab91963e902d6e12f9bc8253be73c48df160
Choi JaeJune <omarchoi80@gmail.com>
2023-02-05 22:44:17 +0900

    로그인 화면 생성

--- GRADUATION PROJECT/MyFrameWork/DreamGame/Resource.h
+++ GRADUATION PROJECT/MyFrameWork/DreamGame/Resource.h
@@ -1,30 +1,34 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
-// 다음에서 사용 DreamGame.rc
-
-#define IDS_APP_TITLE			103
-
-#define IDR_MAINFRAME			128
-#define IDD_DREAMGAME_DIALOG	102
-#define IDD_ABOUTBOX			103
-#define IDM_ABOUT				104
-#define IDM_EXIT				105
-#define IDI_DREAMGAME			107
-#define IDI_SMALL				108
-#define IDC_DREAMGAME			109
-#define IDC_MYICON				2
-#ifndef IDC_STATIC
-#define IDC_STATIC				-1
-#endif
-// 다음은 새 개체에 사용할 기본값입니다.
+// DreamGame.rc에서 사용되고 있습니다.
 //
+#define IDC_MYICON                      2
+#define IDD_DREAMGAME_DIALOG            102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_DREAMGAME                   107
+#define IDI_SMALL                       108
+#define IDC_DREAMGAME                   109
+#define IDR_MAINFRAME                   128
+#define IDD_LogIn                       129
+#define IDB_BITMAP1                     139
+#define IDOK1                           1003
+#define IDC_EDIT1                       1004
+#define IDC_EDIT2                       1005
+#define IDC_EDIT3                       1006
+#define IDC_GameName                    1007
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NO_MFC					130
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_COMMAND_VALUE		32771
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		110
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        140
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1013
+#define _APS_NEXT_SYMED_VALUE           110
 #endif
 #endif
