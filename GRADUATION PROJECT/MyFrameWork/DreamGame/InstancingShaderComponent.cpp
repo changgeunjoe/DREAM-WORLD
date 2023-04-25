af005e26493011756bcf845dd046c121e991e0fa
changgeunjoe <hoi000115@gmail.com>
2023-04-25 15:50:19 +0900

    "인스턴싱 추가"

--- GRADUATION PROJECT/MyFrameWork/DreamGame/InstancingShaderComponent.cpp
+++ GRADUATION PROJECT/MyFrameWork/DreamGame/InstancingShaderComponent.cpp
@@ -26,7 +26,7 @@ D3D12_INPUT_LAYOUT_DESC InstancingShaderComponent::CreateInputLayout(int nPipeli
 
 	return(d3dInputLayoutDesc);
 }
-
+//ss
 D3D12_SHADER_BYTECODE InstancingShaderComponent::CreateVertexShader(int nPipelineState)
 {
 	return(CompileShaderFromFile(L"Shaders.hlsl", "VSInstancing", "vs_5_1", &m_pd3dVertexShaderBlob));
@@ -35,4 +35,38 @@ D3D12_SHADER_BYTECODE InstancingShaderComponent::CreateVertexShader(int nPipelin
 D3D12_SHADER_BYTECODE InstancingShaderComponent::CreatePixelShader(int nPipelineState)
 {
 	return(CompileShaderFromFile(L"Shaders.hlsl", "PSUITextured", "ps_5_1", &m_pd3dPixelShaderBlob));
+}
+void InstancingShaderComponent::CreateShaderVariables(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList)
+{
+
+	m_pd3dcbGameObjects = ::CreateBufferResource(pd3dDevice, pd3dCommandList, NULL,
+sizeof(VS_VB_INSTANCE) * m_nObjects, D3D12_HEAP_TYPE_UPLOAD,
+		D3D12_RESOURCE_STATE_GENERIC_READ, NULL);
+	m_pd3dcbGameObjects->Map(0, NULL, (void **)&m_pcbMappedInsGameObjects);
+}
+void InstancingShaderComponent::UpdateShaderVariables(ID3D12GraphicsCommandList* pd3dCommandList)
+{
+	pd3dCommandList->SetGraphicsRootShaderResourceView(2,
+		m_pd3dcbGameObjects->GetGPUVirtualAddress());
+	for (int j = 0; j < m_nObjects; j++)
+	{
+	/*	m_pcbMappedGameObjects[j].m_xmcColor = (j % 2) ? XMFLOAT4(0.5f, 0.0f, 0.0f, 0.0f) :
+			XMFLOAT4(0.0f, 0.0f, 0.5f, 0.0f);
+		XMStoreFloat4x4(&m_pcbMappedGameObjects[j].m_xmf4x4Transform,
+			XMMatrixTranspose(XMLoadFloat4x4(&m_ppObjects[j]->m_xmf4x4World)));*/
+	}
+
+}
+void InstancingShaderComponent::Render(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, int nPipelineState, ID3D12RootSignature* pd3dGraphicsRootSignature)
+{
+
+	ShaderComponent::Render(pd3dCommandList, nPipelineState, pd3dGraphicsRootSignature, false);
+
+	//±Ì¿Ãπˆ∆€ update
+	UpdateShaderVariables(pd3dCommandList);
+
+	//for (int i = 0; i < m_ppObjects.size(); i++) {
+	//	m_ppObjects[i]->ShadowRender(pd3dDevice, pd3dCommandList, pd3dGraphicsRootSignature, true, this);
+	//}
+
 }
--- GRADUATION PROJECT/MyFrameWork/DreamGame/InstancingShaderComponent.h
+++ GRADUATION PROJECT/MyFrameWork/DreamGame/InstancingShaderComponent.h
@@ -10,6 +10,13 @@ class InstancingShaderComponent : public ShaderComponent
 
 		virtual D3D12_SHADER_BYTECODE CreateVertexShader(int nPipelineState);
 		virtual D3D12_SHADER_BYTECODE CreatePixelShader(int nPipelineState);
+		
+		
+		virtual void CreateShaderVariables(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList);
+		virtual void UpdateShaderVariables(ID3D12GraphicsCommandList* pd3dCommandList);
+
+
+		virtual void Render(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, int nPipelineState, ID3D12RootSignature* pd3dGraphicsRootSignature)
 
 };
 
