82903b3731ec84227e78dc555d37969c7d5d0c21
changgeunjoe <hoi000115@gmail.com>
2023-06-27 23:10:05 +0900

    "그림자 수정"

--- GRADUATION PROJECT/MyFrameWork/DreamGame/ShaderComponent.cpp
+++ GRADUATION PROJECT/MyFrameWork/DreamGame/ShaderComponent.cpp
@@ -54,10 +54,10 @@ D3D12_BLEND_DESC ShaderComponent::CreateBlendState(int nPipelineState)
 	::ZeroMemory(&d3dBlendDesc, sizeof(D3D12_BLEND_DESC));
 	d3dBlendDesc.AlphaToCoverageEnable = FALSE;
 	d3dBlendDesc.IndependentBlendEnable = FALSE;
-	d3dBlendDesc.RenderTarget[0].BlendEnable = TRUE;
+	d3dBlendDesc.RenderTarget[0].BlendEnable = FALSE;
 	d3dBlendDesc.RenderTarget[0].LogicOpEnable = FALSE;
-	d3dBlendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_SRC_ALPHA;
-	d3dBlendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_INV_SRC_ALPHA;
+	d3dBlendDesc.RenderTarget[0].SrcBlend = D3D12_BLEND_ONE;
+	d3dBlendDesc.RenderTarget[0].DestBlend = D3D12_BLEND_ZERO;
 	d3dBlendDesc.RenderTarget[0].BlendOp = D3D12_BLEND_OP_ADD;
 	d3dBlendDesc.RenderTarget[0].SrcBlendAlpha = D3D12_BLEND_ONE;
 	d3dBlendDesc.RenderTarget[0].DestBlendAlpha = D3D12_BLEND_ZERO;
@@ -67,7 +67,6 @@ D3D12_BLEND_DESC ShaderComponent::CreateBlendState(int nPipelineState)
 
 	return(d3dBlendDesc);
 
-	return(d3dBlendDesc);
 }
 
 D3D12_DEPTH_STENCIL_DESC ShaderComponent::CreateDepthStencilState(int nPipelineState)
